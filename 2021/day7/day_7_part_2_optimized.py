eb0f3eed53ba445ae1206f2e0b42e743f8c9fa05
DecemberDream <raoul@tcp.io>
2021-12-09 18:26:07 +0200

    Determine when to use np.floor() and np.ceil()

--- 2021/day7/day_7_part_2_optimized.py
+++ 2021/day7/day_7_part_2_optimized.py
@@ -2,5 +2,14 @@ import numpy as np
 
 
 data = np.array([int(c) for c in open("../../input/day_7_data.txt").read().split(",")], dtype=np.int32)
-fuel = np.abs(data - int(np.mean(data))) * (np.abs(data - int(np.mean(data))) + 1) // 2
+
+m = np.mean(data)
+n = len(data)
+k = len(data[data < m])
+
+if m - int(m) > (2 * k - 1) / (2 * n):
+    fuel = np.abs(data - np.ceil(np.mean(data))) * (np.abs(data - np.ceil(np.mean(data))) + 1) // 2
+else:
+    fuel = np.abs(data - np.floor(np.mean(data))) * (np.abs(data - np.floor(np.mean(data))) + 1) // 2
+    
 print(np.sum(fuel))
